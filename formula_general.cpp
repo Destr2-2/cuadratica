f9042d4631046891700542a3f5c9c590251cf985
Destr2-2 <modv081214hgtrmca5@cbtis217.edu.mx>
2024-10-15 08:18:35 -0600

    ::

---
+++ formula_general.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+int main()  {
+    double a,b,c,x1,x2,total;
+    double b2=0, ac2=0, raiz=0, a2=0;
+    cout<<"Digite los valores de a, b y c en ese orden";
+    cin>>a>>b>>c;
+    //se soluciona la parte de b elevada a la 2 adentro de la raiz
+    b2=b*b;
+    //solucionar la parte - 4ac
+    ac2= ((4*a)*c);
+    //solucion a la raiz cuadrada
+    raiz= sqrt(b2 - ac2);
+    //solucion a 2a
+    a2= 2*a;
+    // solucion a b+- raiz
+    x1= (-b + raiz)/a2;
+    x2= (-b - raiz)/a2;
+
+    cout<<endl;
+    cout<<"x1= "<<x1;
+    cout<<endl;
+    cout<<"x2= "<<x2;
+
+    return 0;
+}
