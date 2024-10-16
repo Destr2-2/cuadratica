a671eae1fcc8ce9571d22fafb9e90e4a4f1d29b3
Destr2-2 <modv081214hgtrmca5@cbtis217.edu.mx>
2024-10-16 08:17:11 -0600

    se le agrego comentarios de como funciona y que hace el codigo y se eliminaron los otros comentarios

--- formula_general.cpp
+++ formula_general.cpp
@@ -1,24 +1,24 @@
+//el codigo hace una operacion de formula general
+//primero soluciona la parte que esta dentro de la raiz cuadrada la cual seria
+// b² - 4ac
+// despues solucionar la raiz cuadrada
+// y al final solucionar b+- (la raiz cuadreada)/2a
+
+
 #include <bits/stdc++.h>
 #include <cmath>
 using namespace std;
 int main()  {
-    // se pone las variables
     double a,b,c,x1,x2,total;
     double b2=0, ac2=0, raiz=0, a2=0;
     cout<<"Digite los valores de a, b y c en ese orden";
     cin>>a>>b>>c;
-    //se soluciona la parte de b elevada a la 2 adentro de la raiz
     b2=b*b;
-    //solucionar la parte - 4ac
     ac2= ((4*a)*c);
-    //solucion a la raiz cuadrada
     raiz= sqrt(b2 - ac2);
-    //solucion a 2a
     a2= 2*a;
-    // solucion a b+- raiz
     x1= (-b + raiz)/a2;
     x2= (-b - raiz)/a2;
-    // se pone las respuestas de x1 x2
     cout<<endl;
     cout<<"x1= "<<x1;
     cout<<endl;
